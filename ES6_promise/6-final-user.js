6f1a96c11ae6a099c705115845a5fa2fd1ac310a
Klajdimuzhaqi <klajdimuzhaqi8@gamil.com>
2026-01-22 16:57:28 +0100

    update

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,10 +1,16 @@
-import signUp from './4-user-promise';
+import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handProfileSignup(firstName , lastName , fileName) {
-    const promises = [signUpUser(firstName , lastName ) uploadPhoto (fileName)];
-
-    return Promise.allSettled{promises}.then{(result) => {
-        console.log(result);
-    }};
+export default function handleProfileSignup(firstName, lastName, fileName) {
+    return Promise.allSettled([
+        signUpUser(firstName, lastName),
+        uploadPhoto(fileName),
+    ]).then((results) =>
+        results.map((result) => {
+            if (result.status === 'fulfilled') {
+                return { status: result.status, value: result.value };
+            }
+            return { status: result.status, value: result.reason };
+        })
+    );
 }
