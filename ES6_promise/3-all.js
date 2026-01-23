75f1654bb247c2d594b0a8cd3d806c88f0f754fb
Klajdimuzhaqi <klajdimuzhaqi8@gamil.com>
2026-01-23 10:42:25 +0100

    update

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -6,6 +6,6 @@ export default function handleProfileSignup() {
             console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
         })
         .catch(() => {
-            console.log("Signup system offline");
+            console.log('Signup system offline');
         });
 }
