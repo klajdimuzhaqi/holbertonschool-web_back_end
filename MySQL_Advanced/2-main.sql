70fc372d786be68cb00ecc12f248a6517e1fc288
Klajdimuzhaqi <klajdimuzhaqi8@gamil.com>
2026-03-04 10:20:39 +0100

    update

---
+++ MySQL_Advanced/2-main.sql
@@ -0,0 +1,7 @@
+-- 2-main.sql
+-- First, let's import the metal_bands.sql file into the database (this assumes it's a valid file)
+-- Run the metal_bands.sql dump
+source /path/to/metal_bands.sql;
+
+-- Then, execute the fan ranking query from 2-fans.sql
+source /path/to/2-fans.sql;
