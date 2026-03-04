504d3ec8a9c21fb6630f77ff708dace5c41d49a7
Klajdimuzhaqi <klajdimuzhaqi8@gamil.com>
2026-03-04 10:39:27 +0100

    update

--- MySQL_Advanced/10-div.sql
+++ MySQL_Advanced/10-div.sql
@@ -1,12 +1,15 @@
--- Create a function and divides two numbers
-DELIMITER $
+-- creates a function SafeDiv that divides a by b and returns 0 if b = 0
+DELIMITER $$
+
 CREATE FUNCTION SafeDiv(a INT, b INT)
 RETURNS FLOAT
+DETERMINISTIC
 BEGIN
-    IF b != 0 THEN
-        RETURN a / b;
-    ELSE
+    IF b = 0 THEN
         RETURN 0;
+    ELSE
+        RETURN a / b;
     END IF;
-END;
-$
+END $$
+
+DELIMITER ;
